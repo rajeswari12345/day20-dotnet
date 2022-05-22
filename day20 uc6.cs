0746232d3ea87b98535c3df8713d94b24c8b7b29
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-22 19:08:52 +0530

    day20 uc6

---
+++ day20 uc6.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using system.Reflections;
+{
+public static string InvokeAnalyseMethod(string message , string Method Name)
+{
+    try
+    {
+        Type type = type.GetType("MoodAnalyzerApp , MoodAnalyse");
+        object moodAnalyseObject = MoodAnalyseFactory.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyzerApp, MoodAnalyse ,
+          " MoodAnalyse", message ");
+          MethodInfo analyseMoodInfo = Type.GetMethod(MethodName);
+        object mood = analyseMoodInfo.Invoke(moodAnalyseObject, Null);
+        return mood.ToString();
+    }
+    catch (NullReferenceException)
+    {
+        throw new MoodAnalysisException(MoodAnalysisException.Exception.No_SUCH_METHOD, "method is not found");
+    }
+}
