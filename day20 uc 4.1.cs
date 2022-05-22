8656d744579cf993e99fd58e40e11203f60e45e3
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-22 19:11:28 +0530

    day20 4.1

---
+++ day20 uc 4.1.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public static  void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+{
+    Object expeected = new MoodAnalyse();
+    object obj = MoodAnalyseFactory.CreateMoodAnalyse(" MoodAnalyzerApp.MoodAnalyse", "MoodAnalyze");
+    expected.Equals(obj);
+}
+
