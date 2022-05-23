23e72c8a626596326b8888191d4cbebab959d66e
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-23 23:19:09 +0530

    day20 uc 1.1

--- day20 uc 1.1.cs
+++ day20 uc 1.1.cs
@@ -1,11 +1,23 @@
-namespace TestProject1
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MoodAnalyzerMSTestWithCore
 {
-    [TestClass]
-    public class UnitTest1
+    public class unitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        public void GivenSadMoodShouldReturnSAD()
         {
+            //Arrange
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+            MoodAnalyser moodanalyse = new MoodAnalyzer(message);
+
+            //Act
+            string mood = moodAnalze.AnalyseMood();
+
+            //Assert 
+            Assert.AreEqual(expected, mood);
         }
     }
+
 }
