39abe771072381332733a6234000df3ca84b299d
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-23 23:10:20 +0530

    day20 uc2

---
+++ day20 uc2.cs
@@ -0,0 +1,31 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using System;
+namespace MoodAnalyzerAppWithCore
+{
+    public class MoodAnalyzer
+    {
+        private string message;
+
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyzeMood()
+        {
+            if (this.message.Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+            catch
+
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
