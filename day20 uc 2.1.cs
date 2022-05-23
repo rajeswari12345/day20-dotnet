afe313f975e847baea5eeb4d5b77e6feb7857a33
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-23 23:13:30 +0530

    day20 uc 2.1

---
+++ day20 uc 2.1.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+{
+namespace MoodAnalyzerMSTestWithCore
+{
+;
+///[DataRow["i am in HAPPY Mood"]]
+[DataRow(null)) 
+namespace MoodAnalyzerMSTestWithCore
+{
+    public class unitTest1
+    {
+        public void GivenSadMoodShouldReturnHAPPY()
+        {
+            //Arrange
+            string expected = "HAPPY";
+            string message = "I am in happy Mood";
+            MoodAnalyser moodanalyse = new MoodAnalyser(message);
+            //Act
+            string mood = moodAnalyse.AnalyseMood();
+            //Assert 
+            Assert.AreEqual(expected, mood);
+        }
+    }
+}
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+{
+namespace MoodAnalyzerMSTestWithCore
+{
+    
